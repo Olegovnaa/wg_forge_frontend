fdd2bc2748419a467a4411a35969bd503c2dcd54
tovpeko <sashatovpeko@mail.ru>
2019-03-07 16:57:13 +0300

    4-6 task end

--- src/app.js
+++ src/app.js
@@ -38,7 +38,14 @@ function starConverter(str) {
 function pad(n) {
     return n < 10 ? '0' + n : n;
 }
-
+function median1(values) {
+    values.sort(function (a, b) {
+        return a - b;
+    });
+    let half = Math.floor(values.length / 2);
+    if (values.length % 2) return values[half]; else return (values[half - 1] + values[half]) / 2.0;
+}
+let list1= []; // массив for midian
 export default (function () {
     // YOUR CODE GOES HERE
     // make an associative array of users to search by ID
@@ -51,20 +58,31 @@ export default (function () {
     for(let i = 0; i < companies.length; i++) {
         companyById[companies[i].id] = companies[i];
     }
+    // make an associative array of users to search by ID
+    let ordersById = {};
+    for(let i = 0; i < orders.length; i++) {
+        ordersById[orders[i].total] = orders[i];
+    }
+
+    let orderCount = 0;// order count
+    let sum = 0; // count of cash
+    let sumAverCheckMale = 0; //count of male
+    let sumAverCheckFemale =0; // count of female
 
     let tableLoad= '<table id="grid" class="table table-bordered table-hover"><thead class="thead-dark">';
-    tableLoad+='<thead>';
-    tableLoad+='<tr>';
-    tableLoad+='<th>Transaction ID</th>';
-    tableLoad+='<th>User Info</th>';
-    tableLoad+='<th>Order Date</th>';
-    tableLoad+='<th>Order Amount</th>';
-    tableLoad+='<th>Card Number</th>';
-    tableLoad+='<th>Card Type</th>';
-    tableLoad+='<th>Location</th>';
-    tableLoad+='</tr>';
-    tableLoad+='</thead>';
-    tableLoad+='<tbody>';
+    tableLoad +='<tr> <th>Search:</th> <th colspan="6"><input type="text" id="search" value="" onkeyup="tableSearch()"></th> </tr>';
+    tableLoad +='<tr>';
+    tableLoad +='<th data-name="Transaction ID" data-type="string" >Transaction ID</th>';
+    tableLoad +='<th data-name="User Info" data-type="name">User Info</th>';
+    tableLoad +='<th data-name="Order Date" data-type="date">Order Date</th>';
+    tableLoad +='<th data-name="Order Amount<" data-type="number">Order Amount</th>';
+    tableLoad +='<th>Card Number</th>';
+    tableLoad +='<th data-name="Card Type" data-type="string">Card Type</th>';
+    tableLoad +='<th data-name="Location" data-type="string">Location</th>';
+    tableLoad +='</tr>';
+    tableLoad +='<tr class="no-result"> <td>Nothing found</td> </tr>';
+    tableLoad +='</thead>';
+    tableLoad +='<tbody>';
 
     for(let i =0; i <orders.length;i++) {
         const orderId = Number(orders[i].id); // переменная для заказов
@@ -72,7 +90,7 @@ export default (function () {
         const userId = Number(orders[i].user_id);
         const createdAt = String(orders[i].created_at);
         let totalPay = roundTwo(orders[i].total);
-        // list1.push(totalPay); // передаем в массив значения
+        list1.push(totalPay); // передаем в массив значения
         const cardNumber = starConverter(String(orders[i].card_number));
         const cardType = String(orders[i].card_type);
         const orderCountry = String(orders[i].order_country);
@@ -103,10 +121,55 @@ export default (function () {
             tableLoad += ' <td>' + cardType + '</td>';
             tableLoad += ' <td>' + orderCountry + ' (' + orderIp + ')<td>';
             tableLoad += '</tr>';
+            // counters of statistika
+            orderCount++;
+            sum+=totalPay; // считаем сумму заказов
+            if(user.gender === "Male"){
+                sumAverCheckMale++
+            }
+            else if(user.gender === "Female"){
+                sumAverCheckFemale++
+            }
         }
     }
     tableLoad+='</tbody>';
     tableLoad+= '<table>';
+    let averCheck = (sum / orderCount); // получаем средий чек
+    tableLoad +='<table class="table">';
+    tableLoad +='<tbody>';
+    tableLoad +='<tr>' +
+        '<th>Orders Count</th>' +
+        '<td>'+ orderCount+'</td>' +
+        '<td colspan="5">&nbsp;</td>' +
+        '</tr>';
+    tableLoad +='<tr>' +
+        '<th>Orders Total</th>' +
+        '<td>$'+roundTwo(sum)+'</td>' +
+        '<td colspan="5">&nbsp;</td>' +
+        '</tr>';
+    tableLoad +='<tr>' +
+        '<th>Median Value</th>' +
+        '<td>$'+median1(list1)+'</td>' +
+        '<td colspan="5">&nbsp;</td>' +
+        '</tr>';
+    tableLoad +='<tr>' +
+        '<th>Average Check</th>' +
+        '<td>$'+roundTwo(averCheck)+'</td>' +
+        '<td colspan="5">&nbsp;</td>' +
+        '</tr>';
+    tableLoad +='<tr>' +
+        '<th>Average Check (Female)</th>' +
+        '<td>'+sumAverCheckFemale+'</td>' +
+        '<td colspan="5">&nbsp;</td>' +
+        '</tr>';
+    tableLoad +='<tr>' +
+        '<th>Average Check (Male)</th>' +
+        '<td>'+sumAverCheckMale+'</td>' +
+        '<td colspan="5">&nbsp;</td>' +
+        '</tr>';
+    tableLoad +='</tbody>';
+    tableLoad +='</table>';
+
     document.getElementById('app').innerHTML = "" + tableLoad + " <br>";
 }());
 
